ad5b1992313adf642e6f69d1465a880b0fef2f86
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2020-11-17 18:08:19 +0530

    Photo Management App with new UI Changes

--- app/src/main/java/com/androidclub/photoapp/MainActivity.kt
+++ app/src/main/java/com/androidclub/photoapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.androidclub.photoapp
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.Toolbar
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val retriever = PhotoRetriever()
